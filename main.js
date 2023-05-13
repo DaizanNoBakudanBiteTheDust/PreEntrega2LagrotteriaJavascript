55bb646bbed3b92b10a1125888e078cf669c51d8
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-05-13 14:16:12 -0400

    Se creo el personaje que toma el texto de un formulario para asi poder utilizarlo en el RPG de texto

--- main.js
+++ main.js
@@ -2,8 +2,27 @@
 
 //Objeto personaje a usar
 
+let nombrePj = document.getElementById("nombre");
+nombrePj.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let nombreAventurero = e.target.children;
+    if (nombreAventurero[0].value === ""){
+        let div = document.createElement("div");
+        div.innerHTML = "Campo vacio";
+        document.getElementById("cuadroDeTexto").append(div);
+    }
+    const personaje = {
+        Nombre: nombreAventurero[0].value,
+        hp: 100,
+        ataque: 1
+    }    
+    console.log(personaje);
+});
+
+
+
 // Objeto  Enemigos
 
 // Atacar
 
-// Recibir daño
+// Recibir daño git
