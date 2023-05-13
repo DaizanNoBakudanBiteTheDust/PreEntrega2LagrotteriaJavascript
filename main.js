949acc1b68df9bb8e21ffd7a6abf6065ded246eb
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-05-13 18:14:29 -0400

    Se agregaron añertas con historia, si se vuelve dificil se cambiara a console Log

--- main.js
+++ main.js
@@ -2,25 +2,25 @@
 
 //Objeto personaje a usar
 
-let nombrePj = document.getElementById("nombre");
-nombrePj.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let nombreAventurero = e.target.children;
-    if (nombreAventurero[0].value === "") {
-        alert("No puedes continuar con este campo Vacio")
-    }
-    const personaje = {
-        nombre: nombreAventurero[0].value,
-        hp: 100,
-        ataque: 1
-    }
-});
+alert("Bienvenido al RPG de Texto, en este viaje usaremos la imaginacion para tratar de meternos en un mundo plagado de monstruos, lo ideal seria que nos conocieramos mejor antes de continuar..");
 
-/*
-let div = document.createElement("div");
-        div.innerHTML = "Campo vacio";
-        document.getElementById("cuadroDeTexto").append(div);
-*/
+let nombrePj = prompt("¿Podria decirme su nombre por favor?");
+
+do {
+    alert("disculpa no te he oido bien..");
+    nombrePj = prompt("¿Podria decirme su nombre por favor?");
+}
+while (nombrePj === "") {}
+
+alert("Excelente " + nombrePj + " mi nombre es Kaine no se si te haz enterado pero aqui han pasado muchas cosas, un Dragon ha estado atacando esta aldea y necesitamos de un valiente guerrero para que nos ayude a derrotarlo, actualmente no contamos con nadie que sepa utilzar un arma, pero veo que tu cargas una, quizas puedas ayudarnos derrotando unos slimes que se encuentran en el pueblo");
+
+
+const personaje = {
+    nombre: nombrePj,
+    hp: 100,
+    ataque: 1
+}
+
 // Objeto  Enemigos
 
 class enemigo {
@@ -29,7 +29,7 @@ class enemigo {
             this.hp = hp,
             this.ataque = ataque
     }
-    recibiDaño(){
+    recibiDaño() {
         this.hp = this.hp - 1
     }
 }
@@ -38,7 +38,7 @@ const slime = new enemigo("Slime", 10, 1);
 const esqueleto = new enemigo("Esqueleto", 20, 2);
 const dragon = new enemigo("Dragon", 50, 3);
 
-console.log(slime, esqueleto, dragon);
+
 
 // Atacar
 
