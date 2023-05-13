7c56ea5355640e372ae1932301b5b16558946ee2
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-05-13 16:31:18 -0400

    se agregaron enemigos

--- main.js
+++ main.js
@@ -6,22 +6,39 @@ let nombrePj = document.getElementById("nombre");
 nombrePj.addEventListener("submit", (e) => {
     e.preventDefault();
     let nombreAventurero = e.target.children;
-    if (nombreAventurero[0].value === ""){
-        let div = document.createElement("div");
-        div.innerHTML = "Campo vacio";
-        document.getElementById("cuadroDeTexto").append(div);
+    if (nombreAventurero[0].value === "") {
+        alert("No puedes continuar con este campo Vacio")
     }
     const personaje = {
-        Nombre: nombreAventurero[0].value,
+        nombre: nombreAventurero[0].value,
         hp: 100,
         ataque: 1
-    }    
-    console.log(personaje);
+    }
 });
 
-
-
+/*
+let div = document.createElement("div");
+        div.innerHTML = "Campo vacio";
+        document.getElementById("cuadroDeTexto").append(div);
+*/
 // Objeto  Enemigos
+
+class enemigo {
+    constructor(nombre, hp, ataque) {
+        this.nombre = nombre,
+            this.hp = hp,
+            this.ataque = ataque
+    }
+    recibiDaño(){
+        this.hp = this.hp - 1
+    }
+}
+
+const slime = new enemigo("Slime", 10, 1);
+const esqueleto = new enemigo("Esqueleto", 20, 2);
+const dragon = new enemigo("Dragon", 50, 3);
+
+console.log(slime, esqueleto, dragon);
 
 // Atacar
 
